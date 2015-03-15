2f564e04e7081a2eaf12ac2bb7f17b209cda8a6c
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-03-15 01:35:51 +0100

    add comments and fix file encoding

--- arduino/dhtx_i2c/read_dhtx_i2c.py
+++ arduino/dhtx_i2c/read_dhtx_i2c.py
@@ -1,3 +1,4 @@
+# encoding: utf
 """
 @author     Alexande Rüedlinger <a.rueedlinger.gmail>
 @date       14.03.2015
@@ -9,17 +10,26 @@ from an arduino (I2C slave) which is connected to a DHT22 sensor.
 import smbus
 import time
 
-def word2data(data):
-    msb = data & 0x00FF
-    lsb = data & 0xFF00
-    lsb = lsb >> 8
+def word2data(word):
+    """
+    Converts a word (2 bytes) to a value with two digits of precision.
+    """
+    # I2C kernel functions assume little endian
+    # but arm uses big endian.
+    # Therefore we need to read a stream of bytes on the reverse order.
+    msb = word & 0x00FF # extract msb byte
+    lsb = word & 0xFF00 # extract lsb byte
+    lsb = lsb >> 8      # eliminate lower bits
 
-    sign = msb & 0x80
-    if sign:
-        msb -= 256
-        lsb *= -1
+    sign = msb & 0x80   # extract sign bit
+    if sign: # if negative
+        msb -= 256 # subtract max uint_t value which is 256
+        lsb *= -1  # make the fractial part also negative for latter addition
 
-    value = msb*100 + lsb
+    value = msb*100 + lsb  # combine integer part and fractial part to together
+
+    # Divide by 100 because we received a float value encoded
+    # as a integer using float * 100.
     return value/100.0
 
 
@@ -43,7 +53,6 @@ def main():
             time.sleep(2)
         except IOError:
             pass
-
 
 
 if __name__ == '__main__':
