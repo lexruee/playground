147553578d2b24da6f07f86b0986e3460e982138
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-03-15 01:20:17 +0100

    fix negative temperature handling

--- arduino/dhtx_i2c/read_dhtx_i2c.py
+++ arduino/dhtx_i2c/read_dhtx_i2c.py
@@ -13,26 +13,37 @@ def word2data(data):
     msb = data & 0x00FF
     lsb = data & 0xFF00
     lsb = lsb >> 8
+
+    sign = msb & 0x80
+    if sign:
+        msb -= 256
+        lsb *= -1
+
     value = msb*100 + lsb
     return value/100.0
+
 
 def main():
     i2c_address = 0x04 # i2c address of the arduino
     bus = smbus.SMBus(1)
 
 
-    for x in range(0,10):
-        # read temperature value
-        temperature_word = bus.read_word_data(i2c_address, 0xB0)
-        time.sleep(0.2)
+    for x in range(0,100):
+        try:
+            # read temperature value
+            temperature_word = bus.read_word_data(i2c_address, 0xB0)
+            time.sleep(0.3)
 
-        # read humidity value
-        humidity_word = bus.read_word_data(i2c_address, 0xA0)
+            # read humidity value
+            humidity_word = bus.read_word_data(i2c_address, 0xA0)
 
-        temperature = word2data(temperature_word)
-        humidity = word2data(humidity_word)
-        print "t: %0.2f, h: %0.2f" % (temperature, humidity)
-        time.sleep(2)
+            temperature = word2data(temperature_word)
+            humidity = word2data(humidity_word)
+            print "t: %0.2f, h: %0.2f" % (temperature, humidity)
+            time.sleep(2)
+        except IOError:
+            pass
+
 
 
 if __name__ == '__main__':
